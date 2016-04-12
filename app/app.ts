05ef6fe5973f9583ad9fef72309e2cbab07c3dab
Paul Gschwendtner <paulgschwendtner@me.com>
2016-04-12 19:22:03 +0200

    UPDATE

--- app/app.ts
+++ app/app.ts
@@ -35,6 +35,8 @@ export class VPApp {
           topics: ['global']
         },
         ios: {
+          senderID: "103734954308",
+          topics: ['global'],
           alert: true,
           badge: true,
           sound: true
