0806e6f4c494b959c754ef53e5f24773e932fb87
Paul Thiel <pa_thiel@t-online.de>
2016-04-16 20:29:42 +0200

    update(representation): remove unnecessary methods

--- app/pages/representation/representation.ts
+++ app/pages/representation/representation.ts
@@ -60,31 +60,4 @@ export class RepresentationPage {
       this.nav.setRoot(LoginPage);
     });
   }
-
-  _getTodayDate(): Date {
-    let current = new Date();
-    let dayOfWeek = current.getDay();
-    let add = 0;
-
-    if (dayOfWeek === 0) add = -2;
-    else if (dayOfWeek === 6) add = -1;
-
-    current.setDate(current.getDate() + add);
-
-    return current;
-  }
-
-  _getTomorrowDate(): Date {
-    let current = new Date();
-    let dayOfWeek = current.getDay();
-    let add = 1;
-
-    if (dayOfWeek === 5) add = 3;
-    else if (dayOfWeek === 6) add = 2;
-
-    current.setDate(current.getDate() + add);
-
-    return current;
-  }
-
 }
