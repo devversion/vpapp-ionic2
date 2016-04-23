751b5024ccd3461a6be922b8887b6e77ad548872
Paul Thiel <pa_thiel@t-online.de>
2016-04-23 11:02:36 +0200

    update(icons): update representation icons to svgs (#29)

--- app/pipes/ToIconPipe.ts
+++ app/pipes/ToIconPipe.ts
@@ -7,12 +7,12 @@ export class ToIconPipe implements PipeTransform {
 
   transform(value: number, args: any[]): string {
     switch (value) {
-      case 1: return "images/ic_find_replace_black_24dp.png";
-      case 2: return "images/ic_do_not_disturb_alt_black_24dp.png";
-      case 3: return "images/ic_arrow_forward_black_24dp.png";
-      case 4: return "images/ic_call_merge_black_24dp.png";
-      case 5: return "images/ic_timelapse_black_24dp.png";
-      default: return "images/ic_help_outline_black_24dp.png";
+      case 1: return "images/ic_find_replace_black_24px.svg";
+      case 2: return "images/ic_do_not_disturb_alt_black_24px.svg";
+      case 3: return "images/ic_arrow_forward_black_24px.svg";
+      case 4: return "images/ic_call_merge_black_24px.svg";
+      case 5: return "images/ic_timelapse_black_24px.svg";
+      default: return "images/ic_help_outline_black_24px.svg";
     }
   }
 
