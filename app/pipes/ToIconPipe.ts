475385f90633114ffb7a87f77269e70710f51c13
Paul Thiel <pa_thiel@t-online.de>
2016-05-07 19:18:17 +0200

    fix(ToIconPipe): fix path to representation icons (#34)

--- app/pipes/ToIconPipe.ts
+++ app/pipes/ToIconPipe.ts
@@ -8,8 +8,8 @@ export class ToIconPipe implements PipeTransform {
   transform(value: number, args: any[]): string {
     switch (value) {
       case 1: return "images/ic_find_replace_black_24px.svg";
-      case 2: return "images/ic_do_not_disturb_alt_black_24px.svg";
-      case 3: return "images/ic_arrow_forward_black_24px.svg";
+      case 2: return "images/ic_block_black_24px.svg";
+      case 3: return "images/ic_trending_flat_black_24px.svg";
       case 4: return "images/ic_call_merge_black_24px.svg";
       case 5: return "images/ic_timelapse_black_24px.svg";
       default: return "images/ic_help_outline_black_24px.svg";
